0b751748eb98f8da98ccf403762be9ca77d1c63a
ryosuke-horie <ryosuke03140@gmial.com>
2023-07-30 21:46:15 +0900

    dockerで開発用DB構築

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',       // DBの種類
+      port: 5432,             // 使用ポート
+      database: 'postgres',   // データベース名
+      host: 'localhost',      // DBホスト名
+      username: 'postgres',   // DBユーザ名
+      password: 'password',   // DBパスワード
+      synchronize: true,      // モデル同期(trueで同期)
+      entities: [__dirname + '/**/*.entity.{js,ts}'],  // ロードするエンティティ
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
