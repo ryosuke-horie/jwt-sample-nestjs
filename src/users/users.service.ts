bc2710b01dbe2a3a37c1473ef2af4ef42a55fcd0
ryosuke-horie <ryosuke03140@gmial.com>
2023-07-30 21:57:35 +0900

    UsersServiceの実装

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,21 @@
+import { Repository } from 'typeorm';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Users } from './users.entity';
 
+/**
+ * @description User情報を扱うクラス
+ */
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(Users)
+        private readonly usersRepository: Repository<Users>,
+    ) {}
+
+    // ユーザーを一人返す
+    findOne(username: Users['name']): Promise<Users | undefined> {
+        // typeormからDBにアクセスし、ユーザーを取得する
+        return this.usersRepository.findOne({ where: { name: username }})
+    }
+}
