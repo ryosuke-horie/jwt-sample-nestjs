1961328b37b536794aee90c16e3aca18f7c01def
ryosuke-horie <ryosuke03140@gmial.com>
2023-07-30 22:00:21 +0900

    UsersModuleの実装

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from './users.entity';
 import { UsersService } from './users.service';
 
 @Module({
-  providers: [UsersService]
+  // UsersEntityをUsersServiceで使用できるようにする
+  imports: [TypeOrmModule.forFeature([Users])],
+
+  providers: [UsersService],
+
+  // UsersServiceを他のクラスでも使用できるようにする
+  exports: [UsersService],
 })
 export class UsersModule {}
